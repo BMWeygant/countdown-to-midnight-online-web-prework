8e843fa412a9267f1dafcce45d7793ce8a669a69
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-21 14:46:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
 end
- puts "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -14,5 +14,5 @@ while number > 0
   sleep 1
   number -= 1
 end
- puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
  end
